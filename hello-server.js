9ccb3931700792b58ec93bfce8827748353ee382
iGuest <jonanv@uw.edu>
2014-11-24 12:16:46 -0800

    web server aweseom

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,19 @@
 
 'use strict';
 
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world!');
+});
+
+app.get('/foo', function(req, res){
+    res.send('you requested foo!')
+
+});
+
+app.listen(8080, function() {
+    console.log('listening at labla')
+});
